c68f8de49f595b4fc8267cd22c8afa85beceb9ad
khyunah <yd3085@naver.com>
2022-08-02 15:46:27 +0900

    구매내역, 상품 리뷰 모델 생성

---
+++ shop/src/main/java/com/shop/fashion/model/Purchasehistory.java
@@ -0,0 +1,39 @@
+package com.shop.fashion.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Purchasehistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne
+	private Item item;
+	@ColumnDefault("1")
+	private int count;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "userId")
+	private User user;
+	@CreationTimestamp
+	private Timestamp createDate;
+}
